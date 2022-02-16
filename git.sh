907b6471e6a6d30fb9dbb793ae17b45364282930
Exeon <actuallyexeon@protonmail.com>
2022-02-15 18:07:35 -0600

    Barebones

---
+++ git.sh
@@ -0,0 +1,7 @@
+echo "# spring2040" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M master
+git remote add origin https://github.com/spring512/spring2040.git
+git push -u origin master
